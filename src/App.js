7ff593772c3bc13af503b2659376f66fabef4618
sonica29 <ssonicadhoot@gmail.com>
2021-01-04 21:23:02 +0530

    2nd change

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           hello world
+          how are you
         </p>
         <a
           className="App-link"
